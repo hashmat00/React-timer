5ae31f082a7d7c7e443132450adbfd0b9c73d33c
hashmat00 <hashmat00@aol.com>
2016-09-17 12:42:52 +0000

    added https redirect link

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+
+// redirect to https
 app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
